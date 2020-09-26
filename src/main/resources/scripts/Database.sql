bba1d8a14c3e33ba0203fbd7f6007dd8b9aeda2d
yaya <cherifi.yahia@yahoo.fr>
2020-09-26 19:11:57 +0200

    Sql scripts added. README updated. Banner changed. Illustration images added to resources. Json data added.

---
+++ src/main/resources/scripts/Database.sql
@@ -0,0 +1,7 @@
+/* Setting up pay_my_buddy_db */
+
+/*
+    You can run this script to create the database.
+    The different tables and columns will be created automatically by the Java Persistence API
+*/
+CREATE DATABASE pay_my_buddy_db;
